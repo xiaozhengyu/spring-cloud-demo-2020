c8a22292385c8a3ca798527331d7cb19e96f73e3
zhengyuxiao <45230851+xiaozhengyu@users.noreply.github.com>
2022-01-04 22:05:08 +0800

    feat: 热部署

--- spring-cloud-demo-2020/cloud-provider-payment/target/classes/sql/table-structure.sql
+++ spring-cloud-demo-2020/cloud-provider-payment/target/classes/sql/table-structure.sql
@@ -3,7 +3,7 @@
 -- ----------------------------
 CREATE TABLE `payment`
 (
-    `id`     bigint(20)                                                  NOT NULL COMMENT '主键',
+    `id`     bigint(20)                                                  NOT NULL AUTO_INCREMENT COMMENT '主键',
     `serial` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '流水号',
     PRIMARY KEY (`id`) USING BTREE
 )
