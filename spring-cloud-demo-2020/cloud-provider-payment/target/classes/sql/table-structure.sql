3af89d23cc963c573a23282cf8a19e3b38dd62ec
zhengyuxiao <45230851+xiaozhengyu@users.noreply.github.com>
2022-01-04 21:37:45 +0800

    fix：解决【Field 'ID' doesn't have a default value】的问题

---
+++ spring-cloud-demo-2020/cloud-provider-payment/target/classes/sql/table-structure.sql
@@ -0,0 +1,9 @@
+-- ----------------------------
+-- Table structure for payment
+-- ----------------------------
+CREATE TABLE `payment`
+(
+    `id`     bigint(20)                                                  NOT NULL COMMENT '主键',
+    `serial` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '流水号',
+    PRIMARY KEY (`id`) USING BTREE
+)
