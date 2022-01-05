8c7f9c65f582a6abd28ec29f1dbd64ab06545ada
zhengyuxiao <45230851+xiaozhengyu@users.noreply.github.com>
2022-01-05 21:44:45 +0800

    feat: 向 Eureka 注册服务

--- spring-cloud-demo-2020/cloud-provider-payment/src/main/java/com/xzy/PaymentApplication.java
+++ spring-cloud-demo-2020/cloud-provider-payment/src/main/java/com/xzy/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.xzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 支付服务
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/1/4 15:35
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
