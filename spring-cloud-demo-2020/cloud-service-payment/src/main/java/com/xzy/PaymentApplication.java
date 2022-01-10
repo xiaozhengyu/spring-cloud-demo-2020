03830717976809502acc97ed61c771c636543396
xiaozy <xiaozy37528@hundsun.com>
2022-01-10 10:57:05 +0800

    feat: 服务注册

--- spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/PaymentApplication.java
+++ spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.xzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 支付服务
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/1/4 15:35
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
