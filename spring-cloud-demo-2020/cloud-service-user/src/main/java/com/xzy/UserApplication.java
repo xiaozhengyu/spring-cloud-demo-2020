8134f18d90586b8a52187a2edb2e555667c91f7d
xiaozy <xiaozy37528@hundsun.com>
2022-01-10 10:34:45 +0800

    refactor: 调整工程结构，为后续配置做准备

---
+++ spring-cloud-demo-2020/cloud-service-user/src/main/java/com/xzy/UserApplication.java
@@ -0,0 +1,17 @@
+package com.xzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户服务
+ *
+ * @author xzy
+ * @date 2022/1/10 10:34
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
