60a1ccd6ed35b7f530c55ef10bafdb181db61d1e
xiaozy <xiaozy37528@hundsun.com>
2022-01-10 12:21:23 +0800

    feat：服务调用

--- spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/controller/PaymentController.java
+++ spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/controller/PaymentController.java
@@ -1,9 +1,12 @@
 package com.xzy.controller;
 
+import com.xzy.msg.MessageBox;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 支付Controller
@@ -14,4 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/payment/payment")
 public class PaymentController {
+    public static final String INVOKE_URL = "http://cloud-user-service";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public PaymentController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/get_user_info")
+    public MessageBox<Object> getUserInfo(@RequestParam("user_id") Long userId) {
+        /*ParameterizedTypeReference<MessageBox<Object>> typeReference = new ParameterizedTypeReference<MessageBox<Object>>() {};
+        ResponseEntity<MessageBox<Object>> responseEntity = restTemplate.exchange(INVOKE_URL + "/user/user/" + userId, HttpMethod.GET, null, typeReference);
+        return responseEntity.getBody();*/
+        
+        return restTemplate.getForObject(INVOKE_URL + "/user/user/" + userId, MessageBox.class);
+    }
 }
