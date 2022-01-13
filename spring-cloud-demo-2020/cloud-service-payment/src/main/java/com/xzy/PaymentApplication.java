89bbfb121cd27a4350b6ee402a6c3ab33e5b8631
zhengyuxiao <zhengyupython@qq.com>
2022-01-13 16:02:32 +0800

    feat：上行服务自己设置服务降级机制

--- spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/PaymentApplication.java
+++ spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/PaymentApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
--- spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/controller/DelayController.java
+++ spring-cloud-demo-2020/cloud-service-payment/src/main/java/com/xzy/controller/DelayController.java
@@ -1,11 +1,17 @@
 package com.xzy.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.xzy.feign.UserServiceFeign;
 import com.xzy.msg.MessageBox;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * 调用处理非常缓慢的RPO
@@ -15,7 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(path = "/payment/delay")
+@Slf4j
 public class DelayController {
+    @Value("${server.port}")
+    private String serverPort;
+
     private final UserServiceFeign userServiceFeign;
 
     @Autowired
@@ -24,18 +34,48 @@ public class DelayController {
     }
 
     /**
-     * 下行服务直接躺平：处理缓慢 + 可能抛出异常
+     * 下行服务直接躺平 + 上行服务直接躺平
      */
-    @GetMapping("/delay")
+    @GetMapping("/delay_with_no_fallback")
     public MessageBox<String> delayRpo() {
         return userServiceFeign.delayRpo();
     }
 
     /**
-     * 下行服务有服务降级机制：在有限时间内返回有效结果
+     * 下行服务直接躺平 + 上行服务设置服务降级机制
      */
-    @GetMapping("/delay_with_fallback")
-    public MessageBox<String> delayRpoWithClientFallback() {
+    @GetMapping("/delay_with_uplink_fallback")
+    @HystrixCommand(fallbackMethod = "planB", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000") // 超时时间
+    })
+    public MessageBox<String> delayRpoWithUplinkServiceFallback() {
+        return userServiceFeign.delayRpo();
+    }
+
+    /**
+     * 下行服务设置服务降级机制 + 上行服务躺平
+     */
+    @GetMapping("/delay_with_downlink_fallback")
+    public MessageBox<String> delayRpoWithDownlinkServiceFallback() {
         return userServiceFeign.delayRpoWithFallback();
+    }
+
+    /**
+     * 下行服务设置服务降级机制 + 上行服务设置服务降级机制
+     */
+    @GetMapping("/delay_with_updown_fallback")
+    @HystrixCommand(fallbackMethod = "planB", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000") // 超时时间
+    })
+    public MessageBox<String> delayRpoWithUplinkAndDownlinkServiceFallback() {
+        return userServiceFeign.delayRpoWithFallback();
+    }
+
+    /*----------fallback method----------*/
+
+    public MessageBox<String> planB() {
+        log.info("处理失败，出现异常或超时，进行服务降级...");
+        String msg = "Server port：" + serverPort + " UUID：" + UUID.randomUUID().toString() + " Current thread：" + Thread.currentThread() + " (Plan B：上行服务)";
+        return MessageBox.ok(msg);
     }
 }
