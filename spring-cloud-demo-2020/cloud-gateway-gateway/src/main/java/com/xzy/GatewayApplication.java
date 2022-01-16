424f14997a99ef4d70fc04c866fbe88daa800fd3
zhengyuxiao <zhengyupython@qq.com>
2022-01-16 23:14:21 +0800

    todo：存在一个BUG待解决

--- spring-cloud-demo-2020/cloud-gateway-gateway/src/main/java/com/xzy/GatewayApplication.java
+++ spring-cloud-demo-2020/cloud-gateway-gateway/src/main/java/com/xzy/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.xzy;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -12,4 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
