8558fe1106070cef95d5b868f03e821e432e6f01
zhengyuxiao <zhengyupython@qq.com>
2022-01-16 22:37:44 +0800

    feat：环境准备

---
+++ spring-cloud-demo-2020/cloud-gateway-gateway/src/main/java/com/xzy/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.xzy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 说明：服务网关
+ *
+ * @author xzy
+ * @date 2022/1/16  22:22
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+}
